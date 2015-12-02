cead21dfc07aefac0852a4a9b79e582281fecde4
Jelle Vandebeeck <jelle@fousa.be>
2015-12-02 08:32:51 +0100

    Add basic project setup.

---
+++ Snapshotting/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Snapshotting
+//
+//  Created by Jelle Vandebeeck on 02/12/15.
+//  Copyright © 2015 Jelle Vandebeeck. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
